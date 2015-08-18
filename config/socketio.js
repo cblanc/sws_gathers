bb2bdac8ebdbb645beb402b7d602debff15c1310
Chris Blanchard <cablanchard@gmail.com>
2015-08-18 12:09:33 +0100

    Tidy up

--- config/socketio.js
+++ config/socketio.js
@@ -9,30 +9,7 @@ var chatController = require("../lib/chat/controller");
 var gatherController = require("../lib/gather/controller");
 var userController = require("../lib/user/controller");
 
-var getRandomUser = callback => {
-	let id = Math.floor(Math.random() * 5000) + 1;
-	client.getUserById({
-		id: id
-	}, (error, response, body) => {
-		if (response.statusCode !== 200) return getRandomUser(callback);
-		return callback(error, response, body);
-	});
-};
-
-var parseCookies = (socket) => {
-	let cookieString = socket.request.headers.cookie;
-	if (typeof cookieString !== 'string') return null;
-	let cookies = socket.request.headers.cookie.split(";")
-				.map(cookie => cookie.trim())
-				.reduce((acc, cookie) => {
-					let values = cookie.split("=");
-					let attr = values[0];
-					let val = values[1];
-					if (attr && val) acc[attr] = val;
-					return acc;
-				}, {})
-	return cookies;
-};
+var parseCookies = EnslClient.parseCookies;
 
 module.exports = io => {
 	var rootNamespace = io.of('/')

4ef01d7a28082ac1039fb4521be9da97796e9f43
Chris Blanchard <cablanchard@gmail.com>
2015-08-28 13:52:01 +0100

    Ignore flags for test

--- config/config.js
+++ config/config.js
@@ -1,21 +1,22 @@
 "use strict";
 
 var env = process.env.NODE_ENV || "development";
+var test = env === "test";
 
 var fs = require("fs");
 var path = require("path");
 
 var baseConfig = require(path.join(__dirname, path.join("environments/" + env.toLowerCase())));
 
-if (process.env.PORT) {
+if (!test && process.env.PORT) {
 	baseConfig.port = parseInt(process.env.PORT, 10);
 }
 
-if (process.env.MONGOLAB_URI) {
+if (!test && process.env.MONGOLAB_URI) {
 	baseConfig.mongo.uri = process.env.MONGOLAB_URI;
 }
 
-if (process.env.RAILS_SECRET) {
+if (!test && process.env.RAILS_SECRET) {
 	baseConfig.secret_token = process.env.RAILS_SECRET;
 }
 

ecd3702085ebea844ea27ab3cc115622f7dc86b3
Chris Blanchard <chris@iddqd.org>
2015-08-31 15:30:33 +0100

    Added hive client

---
+++ spec/hiveClient.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var helper = require("./helpers/index.js");
+var HiveClient = helper.HiveClient;
+var assert = require("chai").assert;
+var fs = require("fs");
+var path = require("path");
+
+describe("ENSL Client", function () {
+	
+});

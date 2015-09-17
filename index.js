57d849f3f050c3129f0eea4730a2171fbb29c3b9
Chris Blanchard <cablanchard@gmail.com>
2015-09-17 20:59:05 +0100

    Tighten update method

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var config = require(path.join(__dirname, "config/config.js"));
+var env = process.env.NODE_ENV || "development";
 
 // Load Models
 require(path.join(__dirname, "db/index"));
 
 // Initialise Steam Bot
-require(path.join(__dirname, "lib/steam/bot"))(config.steamBot);
+if (env !== "test") {
+	require(path.join(__dirname, "lib/steam/bot"))(config.steamBot);
+}
 
 // Configure express
 require(path.join(__dirname, "config/express"))(app);
--- spec/user.js
+++ spec/user.js
@@ -3,6 +3,7 @@
 var helper = require("./helpers/index.js");
 var User = helper.User;
 var assert = require("chai").assert;
+var Profile = helper.Profile;
 var async = require("async");
 var userCount = 0;
 
@@ -44,6 +45,78 @@ describe("User", () => {
 		it ("returns null if no steamid", () => {
 			user.steam.url = null;
 			assert.isNull(user.getSteamId());
+		});
+	});
+
+	describe("#updateProfile", () => {
+		var profile, user;
+
+		beforeEach(done => {
+			user = helper.createUser();
+			Profile.create({
+				userId: user.id
+			}, (error, result) => {
+				if (error) return done(error);
+				profile = result;
+				done();
+			});
+		});
+
+		after(done => helper.clearDb(done));	
+
+		it ("updates profile", done => {
+			var attrs = {
+				enslo: 88,
+				division: "Foo",
+				skill: "Bar"
+			};
+			user.updateProfile(attrs, (error, profile) => {
+				if (error) return done(error);
+				assert.equal(profile.enslo, attrs.enslo);
+				assert.equal(profile.division, attrs.division);
+				assert.equal(profile.foo, attrs.foo);
+				done();
+			});
+		});
+
+		it ("updates abilities", done => {
+			var attrs = {
+				abilities: {
+					"skulk": true,
+					"lerk": true,
+					"fade": true,
+					"gorge": true,
+					"onos": true,
+					"commander": true
+				}
+			};
+			user.updateProfile(attrs, (error, profile) => {
+				if (error) return done(error);
+				for (let attr in attrs.abilities) {
+					assert.isTrue(profile.abilities[attr])
+				}
+				done();
+			});
+		});
+
+		it ("does not update userId", done => {
+			user.updateProfile({
+				userId: 80808080
+			}, (error, result) => {
+				if (error) return done(error);
+				assert.equal(result.userId, user.id);
+				done();
+			});
+		});
+
+		it ("does not update _id", done => {
+			user.updateProfile({
+				_id: "FOOOO"
+			}, (error, result) => {
+				if (error) return done(error);
+				assert.equal(result._id.toString(), profile._id.toString());
+				done();
+			});
 		});
 	});
 });

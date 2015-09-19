faae55f80c9ddc2e8704a5b4897d30085ddf9243
akrigline <andrew@akrigline.com>
2015-09-19 11:54:46 -0400

    Pushed all Styling to be SASS based.
    Added Bootstrap’s SASS build.
    Pulled all original stylesheets into partials and imports.
    Appended Gulpfile with SASS functions (gulp css, gulp watchcss).
    Added Compass dev dependency.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var concat = require("gulp-concat");
 var watch = require("gulp-watch");
 var babel = require("gulp-babel");
 var plumber = require("gulp-plumber");
+var compass = require('gulp-compass');
+
 
 gulp.task('default', ['compile']);
 
@@ -18,4 +20,25 @@ gulp.task('compile', function () {
 
 gulp.task('watch', function () {
 	gulp.watch('lib/react/**', ['compile']);
+});
+
+// Run Compass on SCSS Files
+gulp.task('css', function() {
+  gulp.src('public/css/sass')
+    .pipe(compass({
+        config_file: 'config/config.rb',
+        css: 'public/css',
+        sass: 'public/css/sass'
+      })
+    )
+    .on('error', function(error) {
+      // Spits error out 
+      console.log(error);
+      this.emit('end');
+    })
+    .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('watchcss', function() {
+   gulp.watch('public/css/**/*.scss', ['css']);
 });

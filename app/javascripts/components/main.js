150e6ce0ea2d73a074428d8429e06676c438d4e6
Chris Blanchard <cablanchard@gmail.com>
2016-03-19 13:59:48 +0000

    Fix voting

--- app/javascripts/components/main.js
+++ app/javascripts/components/main.js
@@ -362,6 +362,7 @@ const GatherPage = React.createClass({
 	onGatherSelected(gatherName) {
 		let gather = this.state.gatherPool[gatherName];
 		if (gather === undefined) return;
+		this.updateTitle();
 		this.setState({
 			currentGather: gather.type
 		});

e8afc5da4c22333e9af539ec3ae3e34677db2a79
Absurdon <peter@brodkorb-web.de>
2017-04-18 14:44:21 +0200

    changed links to use https properly

--- config/environments/production.js
+++ config/environments/production.js
@@ -9,8 +9,8 @@ var config = {
 	session_store_name: "_ENSL_session_key",
 	hive_url: "http://hive.naturalselection2.com",
 	hive2_url: "http://hive2.ns2cdt.com",
-	ensl_url: "http://www.ensl.org/",
-	ensl_rules_url: "http://www.ensl.org/articles/464",
+	ensl_url: "https://www.ensl.org/",
+	ensl_rules_url: "https://www.ensl.org/articles/464",
 	steam_bot_link: "http://steamcommunity.com/id/nslgathers"
 };
 
